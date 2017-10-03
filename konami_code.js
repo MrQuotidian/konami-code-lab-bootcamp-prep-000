3db02c92594fc246e9e3eba8c424f461414cbea4
Dan Daly <dan.john.daly@gmail.com>
2017-10-03 21:14:17 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,20 +2,22 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
-  function onKeyDownHandler(e) {
-    const key = parseInt(e.detail || e.which);
-    console.log(key)
+  
+}
 
-    if (key === code[index]) {
-      index++;
+function onKeyDownHandler(e) {
+  const key = parseInt(e.detail || e.which);
+  console.log(key)
 
-      if (index === code.length) {
-        alert("Hurray!");
+  if (key === code[index]) {
+    index++;
 
-        index = 0;
-      }
-    } else {
+    if (index === code.length) {
+      alert("Hurray!");
+
       index = 0;
     }
+  } else {
+    index = 0;
   }
 }
