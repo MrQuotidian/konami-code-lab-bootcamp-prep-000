54eaa2859d9b5d8baa112a72ae25052fcc0b57d9
Dan Daly <dan.john.daly@gmail.com>
2017-10-03 21:12:43 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -4,13 +4,13 @@ function init() {
   // Write your JavaScript code inside the init() function
   function onKeyDownHandler(e) {
     const key = parseInt(e.detail || e.which);
-   
+
     if (key === code[index]) {
       index++;
-   
+
       if (index === code.length) {
         alert("Hurray!");
-   
+
         index = 0;
       }
     } else {
